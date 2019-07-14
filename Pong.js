3cc09702f5da98f7671b528cae3c8fca9a30b804
barney54321 <andrewesteban1999@gmail.com>
2019-07-14 12:48:14 +1000

    Added starter files

---
+++ Pong.js
@@ -0,0 +1,9 @@
+var oneCvs = document.getElementById("one");
+var twoCvs = document.getElementById("two");
+var threeCvs = document.getElementById("three");
+var fourCvs = document.getElementById("four");
+var one = oneCvs.getContext("2d");
+var two = twoCvs.getContext("2d");
+var three = threeCvs.getContext("2d");
+var four = fourCvs.getContext("2d");
+
